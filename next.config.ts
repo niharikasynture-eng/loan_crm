14e66d6383ab1d49ab6d76153c67de910a6522df
Ankita856 <ankitachougule856@gmail.com>
2026-08-12 11:12:39 +0530

    Allow local dev origin for mobile testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['10.91.210.46', 'localhost:3000'],
 };
 
 export default nextConfig;
